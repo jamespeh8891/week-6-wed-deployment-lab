16ea3a1599ccb5964af6eaa60ba87d417599a905
James Pehrson <jamesbpehrson88@gmail.com>
2022-06-29 15:04:34 -0600

    second push

--- server/index.js
+++ server/index.js
@@ -6,3 +6,9 @@ const app = express()
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
+
+const port = process.env.PORT || 4005
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
