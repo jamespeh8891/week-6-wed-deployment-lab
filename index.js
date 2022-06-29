e6285acf8a14b2de00d622e3bce63bbc6afc7bcb
James Pehrson <jamesbpehrson88@gmail.com>
2022-06-29 16:03:19 -0600

    third push

---
+++ index.js
@@ -0,0 +1,7 @@
+const downloadBtn = document.querySelector('#download-btn')
+
+const message = () => {
+    alert('You got pranked, no fortnite for you. Now installing 74 viruses to this computer.')
+}
+
+downloadBtn.addEventListener('click', message)
--- server/index.js
+++ server/index.js
@@ -7,6 +7,14 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
+app.get('/css', (req, res) => {
+    res.sendFile(path.join(__dirname, '../styles.css'))
+})
+
+app.get('/js', (req, res) => {
+    res.sendFile(path.join(__dirname, '../index.js'))
+})
+
 const port = process.env.PORT || 4005
 
 app.listen(port, () => {
